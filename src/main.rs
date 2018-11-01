374c1028f3ce7527b63318098b2955e866791cda
hatoo <hato2000@gmail.com>
2018-11-01 20:40:36 +0900

    Setup Termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate termion;
+
+use std::io::{stdin, stdout, Write};
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+use termion::event::{Event, Key};
+use termion::input::{MouseTerminal, TermRead};
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = MouseTerminal::from(AlternateScreen::from(stdout()).into_raw_mode().unwrap());
+    let (tx, rx) = channel();
+
+    thread::spawn(move || {
+        for c in stdin.events() {
+            if let Ok(evt) = c {
+                tx.send(evt).unwrap();
+            }
+        }
+    });
+
+    loop {
+        if let Ok(evt) = rx.recv_timeout(Duration::from_millis(16)) {
+            if evt == Event::Key(Key::Ctrl('c')) {
+                return;
+            }
+        }
+        stdout.flush().unwrap();
+    }
 }
