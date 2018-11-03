33ebbdc2de91e95628c8b2727c7466304bd1aec7
hatoo <hato2000@gmail.com>
2018-11-03 16:14:48 +0900

    cat

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+extern crate clap;
 extern crate termion;
 
+use clap::{App, Arg};
+use std::ffi::OsStr;
+use std::fs;
 use std::io::{stdin, stdout, Write};
+use std::path;
 use termion::clear;
 use termion::cursor;
 use termion::event::{Event, Key};
@@ -9,6 +14,31 @@ use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 
 fn main() {
+    // Clap
+    let matches = App::new("kiro")
+        .about("A text editor")
+        .bin_name("kiro")
+        .arg(Arg::with_name("file"))
+        .get_matches();
+
+    // ファイルパスはUTF-8でない可能性があるのでOsStrを使います
+    let file_path: Option<&OsStr> = matches.value_of_os("file");
+
+    // テキストを読み込む
+    // 改行コードに関してはlinesに一任している
+    let buffer: Vec<Vec<char>> = file_path
+        .and_then(|file_path| {
+            // エラー処理は適当
+            fs::read_to_string(path::Path::new(file_path))
+                .ok()
+                .map(|s| {
+                    s.lines()
+                        .map(|line| line.trim_right().chars().collect())
+                        .collect()
+                })
+        })
+        .unwrap_or(Vec::new());
+
     let stdin = stdin();
     // Rawモードに移行
     // into_raw_modeはIntoRawModeトレイトに定義されている
@@ -20,8 +50,16 @@ fn main() {
     write!(stdout, "{}", clear::All);
     // カーソルを左上に設定する(1-indexed)
     write!(stdout, "{}", cursor::Goto(1, 1));
-    // Hello World!
-    write!(stdout, "Hello World!");
+
+    // bufferの内容を出力する
+    for line in &buffer {
+        for &c in line {
+            write!(stdout, "{}", c);
+        }
+        // Rawモードでは開業は\r\nで行う
+        write!(stdout, "\r\n");
+    }
+
     // 最後にフラッシュする
     stdout.flush().unwrap();
 
